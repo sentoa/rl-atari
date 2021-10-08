7ba5915f4f9bce1cce7f5f54090eaba063441691
Peter K <pkjar16@student.aau.dk>
2021-10-08 13:22:08 +0200

    Update deepq.py

--- deepq.py
+++ deepq.py
@@ -1,7 +1,7 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import atari_wrappers, wrappers
+from atari_wrappers import make_atari, wrap_deepmind
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
