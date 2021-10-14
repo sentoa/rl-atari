a97b85318d7f953b21988b8f7079f66f67a62562
Peter Kjaer <peter@servon.co>
2021-10-14 13:52:46 +0200

    Working example, need to integrate into DQN

--- descriminator_test_env.py
+++ descriminator_test_env.py
@@ -10,6 +10,10 @@ import os
 import PIL
 from tensorflow.keras import layers
 import time as t
+import math
+
+print(tf.executing_eagerly())
+tf.config.run_functions_eagerly(True)
 
 def make_generator_model():
     model = tf.keras.Sequential()
@@ -76,11 +80,29 @@ cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 
-def discriminator_loss(real_output, fake_output):
+## TEST REWARD STUFF
+reward_list = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+def discriminator_loss(real_output, fake_output, reward_list):
     # IMAGE LOSS
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
-    total_loss = real_loss + fake_loss
+
+    image_loss = real_loss + fake_loss
+    image_loss = tf.math.log(image_loss)
+
+    # REWARD LOSS
+    reward_avg = np.average(reward_list)
+
+    if reward_avg == 0:
+        reward_loss = tf.constant(1, dtype=tf.float32)
+    else:
+        reward_loss = tf.constant(reward_avg, dtype=tf.float32)
+        reward_loss = tf.math.log(reward_loss)
+
+    # TOTAL LOSS
+    total_loss = image_loss - reward_loss
+    
     return total_loss
 
 checkpoint_dir = 'checkpoints'
@@ -89,16 +111,19 @@ checkpoint = tf.train.Checkpoint(discriminator_optimizer=discriminator_optimizer
                                  discriminator=discriminator)
 
 @tf.function
-def train_step(real_image):
+def train_step(real_image, reward_list):
     noise = tf.random.normal([1, 100])
 
     with tf.GradientTape() as disc_tape:
-        generated_images = model(noise, training=True)
+      
+        generated_images = model(noise, training=False)
 
         real_output = discriminator(real_image, training=True)
         fake_output = discriminator(generated_images, training=True)
 
-        disc_loss = discriminator_loss(real_output, fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output, reward_list)
+        print("LOSS FOR DISCRIMINATOR")
+        tf.print(disc_loss)
 
         gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
         discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
@@ -108,6 +133,5 @@ def train_step(real_image):
 real_image = np.load('test.dat.npy')
 real_image = tf.convert_to_tensor(real_image, dtype=tf.float32)
 real_image = tf.expand_dims(real_image, axis=0)
-
-train_step(real_image)
+train_step(real_image, reward_list)
 checkpoint.save(file_prefix = checkpoint_prefix)
