5717241a292257d709788cc9c090e63680f583de
Peter K <pkjar16@student.aau.dk>
2021-10-27 10:45:17 +0200

    fuk

--- DQN_PROD.py
+++ DQN_PROD.py
@@ -237,7 +237,7 @@ for episode in range(episodes):
     episode_count += 1
 
     # Save Model every 100th episode
-    if(episode_count % 100 == 0 && episode_count > 3000):
+    if(episode_count % 100 == 0 and episode_count > 3000):
         print("Saved model at episode {}".format(episode_count))
         model_path = 'models/episode-{}'.format(episode_count)
 
